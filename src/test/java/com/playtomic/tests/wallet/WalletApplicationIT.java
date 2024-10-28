d5db8f1dd1f37379c5ea43f8f233db79893b25d9
Eka Port <ekaitz.port@gmail.com>
2024-10-28 19:18:45 +0100

    Make not found test pass

--- src/test/java/com/playtomic/tests/wallet/WalletApplicationIT.java
+++ src/test/java/com/playtomic/tests/wallet/WalletApplicationIT.java
@@ -2,16 +2,19 @@ package com.playtomic.tests.wallet;
 
 import com.playtomic.tests.wallet.domain.Wallet;
 import com.playtomic.tests.wallet.domain.WalletId;
+import com.playtomic.tests.wallet.domain.WalletNotFound;
 import com.playtomic.tests.wallet.domain.WalletRepository;
 import com.playtomic.tests.wallet.domain.examples.WalletExamples;
 import com.playtomic.tests.wallet.domain.examples.WalletIdExamples;
 import com.playtomic.tests.wallet.infrastructure.api.WalletController;
 import com.playtomic.tests.wallet.infrastructure.api.WalletResponse;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @ActiveProfiles(profiles = "test")
@@ -31,17 +34,14 @@ public class WalletApplicationIT {
 
         WalletResponse foundWallet = controller.getWallet(id);
 
-        Assertions.assertThat(foundWallet).isNotNull();
-        Assertions.assertThat(foundWallet.id()).isEqualTo(id);
-        Assertions.assertThat(foundWallet.balance()).isEqualTo(savedWallet.balanceAmount());
+        assertThat(foundWallet).isNotNull();
+        assertThat(foundWallet.id()).isEqualTo(id);
+        assertThat(foundWallet.balance()).isEqualTo(savedWallet.balanceAmount());
     }
 
     @Test
     public void should_get_not_found_if_the_wallet_does_not_exist() {
         WalletId id = WalletIdExamples.random();
-        WalletResponse wallet = controller.getWallet(id.asString());
-
-        Assertions.assertThat(wallet).isNotNull();
-        Assertions.assertThat(wallet.id()).isEqualTo(id.asString());
+        assertThrows(WalletNotFound.class, () -> controller.getWallet(id.asString()));
     }
 }
