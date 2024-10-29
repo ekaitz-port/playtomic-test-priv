a1ca00a572d393afbc65d430797ede49073ef135
Eka Port <ekaitz.port@gmail.com>
2024-10-29 20:55:13 +0100

    Add sqls to create data and schema

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO wallet (id, balance)
+VALUES ('5662b86f-adba-4e63-9995-a4f0d3104619', 50.00);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE wallet
+(
+    id      VARCHAR(36) PRIMARY KEY,
+    balance DECIMAL(10, 2) NOT NULL
+);
