01355e7f97869bb5e4736ea70a8a174fee54250e
Eka Port <ekaitz.port@gmail.com>
2024-10-29 17:31:40 +0100

    Test StripeService with MockServer

--- src/test/java/com/playtomic/tests/wallet/infrastructure/service/impl/StripeServiceTest.java
+++ src/test/java/com/playtomic/tests/wallet/infrastructure/service/impl/StripeServiceTest.java
@@ -1,36 +1,99 @@
 package com.playtomic.tests.wallet.infrastructure.service.impl;
 
 
+import com.playtomic.tests.wallet.domain.examples.PaymentIdExamples;
 import com.playtomic.tests.wallet.infrastructure.service.StripeAmountTooSmallException;
-import com.playtomic.tests.wallet.infrastructure.service.StripeServiceException;
 import com.playtomic.tests.wallet.infrastructure.service.StripeService;
-
+import com.playtomic.tests.wallet.infrastructure.service.StripeServiceException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.integration.ClientAndServer;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 import java.net.URI;
 
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+import static org.mockserver.model.JsonBody.json;
+
 /**
  * This test is failing with the current implementation.
- *
+ * <p>
  * How would you test this?
  */
+@SpringBootTest
+@ActiveProfiles(profiles = "test")
 public class StripeServiceTest {
 
-    URI testUri = URI.create("http://how-would-you-test-me.localhost");
+    private ClientAndServer mockServer;
+    URI testUri = URI.create("http://localhost:3333");
     StripeService s = new StripeService(testUri, testUri, new RestTemplateBuilder());
+
+    @BeforeEach
+    public void setUp() {
+        mockServer = ClientAndServer.startClientAndServer(3333);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        mockServer.close();
+    }
 
     @Test
     public void test_exception() {
+        String cardNumber = "4242 4242 4242 4242";
+        BigDecimal amount = new BigDecimal(5);
+        mockUnprocessableEntity(cardNumber, amount);
         Assertions.assertThrows(StripeAmountTooSmallException.class, () -> {
-            s.charge("4242 4242 4242 4242", new BigDecimal(5));
+            s.charge(cardNumber, amount);
         });
     }
 
     @Test
     public void test_ok() throws StripeServiceException {
-        s.charge("4242 4242 4242 4242", new BigDecimal(15));
+        String cardNumber = "4242 4242 4242 4242";
+        BigDecimal amount = new BigDecimal(15);
+        mockOkResponse(cardNumber, amount);
+        s.charge(cardNumber, amount);
+    }
+
+    private void mockUnprocessableEntity(String card, BigDecimal amount) {
+        new MockServerClient("localhost", 3333)
+                .when(
+                        request()
+                                .withMethod("POST")
+                                .withPath("/")
+                                .withHeader("Content-Type", "application/json")
+                                .withBody(json("{ \"credit_card\": \"" + card + "\", \"amount\": " + amount + " }"))
+                )
+                .respond(
+                        response()
+                                .withStatusCode(422)
+                                .withHeader("Content-Type", "application/json")
+                );
+    }
+
+    private void mockOkResponse(String card, BigDecimal amount) {
+        String responseBody = "{\"id\": \"" + PaymentIdExamples.random().value() + "\"}";
+        new MockServerClient("localhost", 3333)
+                .when(
+                        request()
+                                .withMethod("POST")
+                                .withPath("/")
+                                .withHeader("Content-Type", "application/json")
+                                .withBody(json("{ \"credit_card\": \"" + card + "\", \"amount\": " + amount + " }"))
+                )
+                .respond(
+                        response()
+                                .withStatusCode(200)
+                                .withHeader("Content-Type", "application/json")
+                                .withBody(responseBody)
+                );
     }
 }
